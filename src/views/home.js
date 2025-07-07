b623547164525564420df38ca93ba1dcf01a2a10
batark1 <sahdevinfinity@gmail.com>
2025-07-07 23:29:09 +0530

    Create home.js

---
+++ src/views/home.js
@@ -0,0 +1,240 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import { Helmet } from 'react-helmet'
+
+import './home.css'
+
+const Home = (props) => {
+  return (
+    <div className="home-container10">
+      <Helmet>
+        <title>SahdevJaesing.card</title>
+        <meta property="og:title" content="SahdevJaesing.card" />
+      </Helmet>
+      <div data-role="Header" className="home-navbar-container">
+        <div className="home-navbar">
+          <div className="home-container11"></div>
+          <h1 className="home-text10">Sahdev Jaesing</h1>
+        </div>
+      </div>
+      <div className="home-section-separator1"></div>
+      <div className="home-section-separator2"></div>
+      <div className="home-container12">
+        <div className="home-hero">
+          <div className="home-hero-text-container">
+            <h1 className="home-heading1">
+              <span className="home-text11">Not Your Average Guy.</span>
+              <br className="home-text12"></br>
+              <span className="home-text13">
+                CREATIVE. COACH. MODEL. POLYMATH.
+              </span>
+            </h1>
+            <span className="home-text14">
+              Sahdev Jaesing helps people transform — physically, mentally, and
+              aesthetically. His work spans performance, design, and identity,
+              helping you move better, look sharper, and feel like your better
+              self. A personal evolution.
+            </span>
+            <a href="tel:+91 9016343612" className="home-link1">
+              <h2 className="home-heading2">Contact</h2>
+            </a>
+            <a
+              href="mailto:sahdevinfinity@gmail.com?subject="
+              className="home-link2"
+            >
+              <h2 className="home-heading3">Email</h2>
+            </a>
+            <a
+              href="https://www.instagram.com/sahdevjaesing/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="home-link3"
+            >
+              <h2 className="home-heading4">Instagram</h2>
+            </a>
+          </div>
+          <a
+            href="https://www.instagram.com/sahdevjaesing/"
+            target="_blank"
+            rel="noreferrer noopener"
+            className="home-link4"
+          >
+            <img alt="image" src="/img_5498-1500w.jpg" className="home-image" />
+          </a>
+        </div>
+      </div>
+      <div className="home-about">
+        <div className="home-max-content-width-container">
+          <div className="home-heading-container1">
+            <h1 className="home-text15">Creative Facets</h1>
+          </div>
+          <div className="home-content-container1">
+            <div className="home-about11">
+              <div className="home-container13">
+                <svg viewBox="0 0 1024 1024" className="home-icon10">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text16">
+                  <span className="Card-Text">Creative Director</span>
+                  <br></br>
+                  Lorem ipsum dolor sit amet aulla quis sem at nibh elementum
+                  imperdiet.
+                </span>
+              </div>
+              <div className="home-container14">
+                <svg viewBox="0 0 1024 1024" className="home-icon12">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text19">
+                  <span>Fashion Alchemist</span>
+                  <br className="Card-Text"></br>
+                  Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet
+                </span>
+              </div>
+              <div className="home-container15">
+                <svg viewBox="0 0 1024 1024" className="home-icon14">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text22">Biological Architect</span>
+              </div>
+            </div>
+            <div className="home-about12">
+              <div className="home-container16">
+                <svg viewBox="0 0 1024 1024" className="home-icon16">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text23">Artist</span>
+              </div>
+              <div className="home-container17">
+                <svg viewBox="0 0 1024 1024" className="home-icon18">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text24">Digital Tinkerer</span>
+              </div>
+              <div className="home-container18">
+                <svg viewBox="0 0 1024 1024" className="home-icon20">
+                  <path d="M170 512q0-140 101-241t241-101 241 101 101 241-101 241-241 101-241-101-101-241z"></path>
+                </svg>
+                <span className="home-text25">Writer/ Thinker</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-section-separator3"></div>
+      <div className="home-section-separator4"></div>
+      <div className="home-section-separator5"></div>
+      <div className="home-faqs">
+        <h2 className="home-text26 Section-Heading">Most common questions</h2>
+        <div className="home-content-container2">
+          <div className="home-faq1">
+            <div className="home-question-container1">
+              <span className="home-question1">What do you actually do?</span>
+            </div>
+            <div className="home-answer-container1">
+              <span className="home-answer1">A lot actually.</span>
+            </div>
+          </div>
+          <div className="home-faq2">
+            <div className="home-question-container2">
+              <span className="home-question2">
+                Can I collaborate with you?
+              </span>
+            </div>
+            <div className="home-answer-container2">
+              <span className="home-answer2">
+                If your project’s creative, occasionally chaotic, or curious —
+                I’m listening.
+              </span>
+            </div>
+          </div>
+          <div className="home-faq3">
+            <div className="home-question-container3">
+              <span className="home-question3">
+                Can I work with you?/ Can I be your client?
+              </span>
+            </div>
+            <div className="home-answer-container3">
+              <span className="home-answer3">Yes. Text/ DM me.  </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="home-section-separator6"></div>
+      <div className="home-container19">
+        <h1 className="home-text27">Brief about:</h1>
+        <div className="home-heading-container2">
+          <span className="home-text28">
+            <span>
+              Sahdev Jaesing is a performance-driven coach and multidisciplinary
+              creator with over five years of experience in fitness training,
+              athletic development, and physique transformation. He has trained
+              a wide range of clients—from injury rehabilitation cases to
+              professional boxers focused on building strength, agility, and
+              reflexes.
+            </span>
+            <br className="Section-Text"></br>
+            <br className="Section-Text"></br>
+            <span>
+              With a strong foundation in anatomy, nutrition, and
+              performance-enhancing methods, Sahdev designs personalized
+              programs that integrate bodybuilding, calisthenics, powerlifting,
+              and combat conditioning. He is a Junior State-Level Bodybuilding
+              Champion (2024) and is known for his sharp observation,
+              discipline, and ability to build systems that drive long-term
+              results.
+            </span>
+            <br className="Section-Text"></br>
+            <br className="Section-Text"></br>
+            <span>
+              Beyond fitness, Sahdev is also a creator and strategist—developing
+              tools and resources around design, human behavior, and performance
+              psychology to help others evolve physically and mentally.
+            </span>
+            <br className="Section-Text"></br>
+            <br className="Section-Text"></br>
+            <span>
+              As a creative, Sahdev blends aesthetics, emotion, and identity
+              through visuals—crafting content that moves with rhythm and
+              intention. His work spans styling, modeling, and even garment
+              creation, often curating his own wardrobe pieces as expressions of
+              philosophy and form.
+            </span>
+            <br className="Section-Text"></br>
+            <br className="Section-Text"></br>
+            <span>
+              He is also a digital tinkerer — building custom solutions through
+              underrated and looked over tech pieces, constantly merging
+              physical presence with digital identity. Whether it&apos;s
+              painting, music, writing, or developing a new tool, his creative
+              pursuits are rooted in clarity, transformation, and expression.
+            </span>
+            <br className="Section-Text"></br>
+            <br className="Section-Text"></br>
+            <span>
+              At the core of everything, Sahdev is a thinker — a modern-day
+              polymath evolving in public.
+            </span>
+            We will send you our monthly freebies on your email. We will not
+            share your email address or use it for promotional goals.
+          </span>
+        </div>
+        <Link to="/page" className="home-navlink Anchor button">
+          This does Nothing
+        </Link>
+      </div>
+      <div className="home-subscribe"></div>
+      <div className="home-section-separator7"></div>
+      <div className="home-footer-container">
+        <div className="home-footer">
+          <div className="home-copyright-container">
+            <span className="home-link5 Anchor">Sahdev Jaesing</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
